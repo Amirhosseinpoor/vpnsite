4f599213c13b52929e5fa4a86944a61f446ba2a7
amirhpk1818 <amirhosseinpoora6@gmail.com>
2024-07-20 21:26:34 +0330

    add postgresql replace sqlite

--- config/settings.py
+++ config/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vpnsite_database',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
